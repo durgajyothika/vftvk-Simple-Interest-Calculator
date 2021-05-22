f99d05e71a70907f044dfec248a0ebc3d5a6865c
DurgaJyothika <jyothikapinnaboina@gmail.com>
2021-05-22 14:23:01 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ function compute()
     var amount =principal * years * rate /100;
     var year =new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit"+principal+",\<br\>at an interest rate of"
-    +rate+"%\<br\>you will receive an amount of "+amount+",\<br\>in the year"+year+"\<br\>"
-    
     if(principal<=0)
     {
         alert("enter a positive number");
+        document.getElementById("principal").focus();
     }
-
+    else{
+    document.getElementById("result").innerHTML="If you deposit"+principal+",\<br\>at an interest rate of"
+    +rate+"%\<br\>you will receive an amount of "+amount+",\<br\>in the year"+year+"\<br\>"
+    }
 }
 function updateRate()
 {
